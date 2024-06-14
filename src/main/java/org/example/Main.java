34ada6aae16f2b26774703052fdd1ceabd81fdc3
Aka871 <163530203+Aka871@users.noreply.github.com>
2024-06-14 23:36:38 +0900

    feat: 作成したファイルに文章を追加

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,13 +3,15 @@ package org.example;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 public class Main {
 
   public static void main(String[] args) {
     try {
       Path path = Path.of("JavaCourse.txt");
-      Files.writeString(path, "コツコツやろう！");
+      Files.writeString(path, "継続は力なり！", StandardOpenOption.APPEND);
+      System.out.println(Files.readString(path));
     } catch (IOException e) {
 
     }
