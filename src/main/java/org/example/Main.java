84eede8f2b3c02827fd33d25237d8481da464306
Aka871 <163530203+Aka871@users.noreply.github.com>
2024-06-15 00:36:55 +0900

    feat: 例外処理の中にさらに例外処理を追加

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,13 +8,23 @@ import java.nio.file.StandardOpenOption;
 public class Main {
 
   public static void main(String[] args) {
+    Path path = Path.of("JavaCourse.txt");
+
     try {
-      Path path = Path.of("JavaCourse.txt");
       Files.writeString(path, "継続は力なり！", StandardOpenOption.APPEND);
       System.out.println(Files.readString(path));
     } catch (IOException e) {
       e.printStackTrace();
       System.out.println("例外が発生した。");
+
+      //例外処理の中でさらに例外になった場合、どうなるのかを確認する
+      try {
+        Files.writeString(path, "再試行 - 継続は力なり！", StandardOpenOption.APPEND);
+        System.out.println(Files.readString(path));
+      } catch (IOException ex) {
+        ex.printStackTrace();
+        System.out.println("再試行中にさらに例外が発生した！");
+      }
     }
   }
 }
